aa053e76db137b6d062539471073e72ac50508c4
NguyenUyen0904 <nguyen.uyen.0904@gmail.com>
2022-07-15 16:32:34 +0700

    Try run with dev tabist system

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,52 +1,84 @@
 const { test, expect } = require("@playwright/test");
 
-//FIRST TEST
-test("basic test", async ({ page }) => {
-  await page.goto("https://playwright.dev/"); // đến page mình cho sẵn
-  const title = page.locator(".navbar__inner .navbar__title");
-  await expect(title).toHaveText("Playwright"); // đợi đến khi có title có chứa sub-string Playwright
-});
-
-//WRITING ASSERTIONS
-
-test("my test", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-
-  // Expect an attribute "to be strictly equal" to the value.// đợi đến khi định vị của cái text = Get started có thuộc tính href=/docs/intro
-  await expect(page.locator("text=Docs").first()).toHaveAttribute(
-    "href",
-    "/docs/intro"
-  );
-
-  await page.click("text=Docs"); // click vào element có text= Get started
-  // Expect some text to be visible on the page.- đợi đến khi có text Introduction hiển thị
-  await expect(page.locator("text=Introduction").first()).toBeVisible();
-});
-
-//USING TEST HOOKS
-test.describe("feature foo", () => {
-  test.beforeEach(async ({ page }) => {
-    // Go to the starting url before each test.
-    await page.goto("https://playwright.dev/");
-  });
-
-  test("correct page", async ({ page }) => {
-    // Assertions use the expect API.
-    await expect(page).toHaveURL("https://playwright.dev/");
-  });
-
-  test("button Docs", async ({ page }) => {
-    // Expect an attribute "to be strictly equal" to the value.// đợi đến khi định vị của cái text = Get started có thuộc tính href=/docs/intro
-    await expect(page.locator("text=Docs").first()).toHaveAttribute(
-      "href",
-      "/docs/intro"
-    );
-
-    await page.click("text=Docs"); // click vào element có text= Get started
-    // Expect some text to be visible on the page.- đợi đến khi có text Introduction hiển thị
-    await expect(page.locator("text=Introduction").first()).toBeVisible();
-  });
+test('test', async ({ page }) => {
+  // Go to https://direct-channel-web-git-stg-oyojp.vercel.app/h/B13HNEW
+  await page.goto('https://direct-channel-web-git-stg-oyojp.vercel.app/h/B13HNEW');
+  // Click button:has-text("大人 1名・子供 0名 / 1室")
+  await page.locator('button:has-text("大人 1名・子供 0名 / 1室")').click();
+  // Click text=大人1 >> [data-testid="plus-button"]
+  await page.locator('text=大人1 >> [data-testid="plus-button"]').click();
+  // Click text=大人2 >> [data-testid="plus-button"]
+  await page.locator('text=大人2 >> [data-testid="plus-button"]').click();
+  // Click text=大人3 >> [data-testid="plus-button"]
+  await page.locator('text=大人3 >> [data-testid="plus-button"]').click();
+  // Click text=適用
+  await page.locator('text=適用').click();
+  // Click text=合計価格(税込)96,000 円 〜67,200 円 〜詳細・予約 >> button
+  await page.locator('text=合計価格(税込)96,000 円 〜67,200 円 〜詳細・予約 >> button').click();
+  await expect(page).toHaveURL('https://direct-channel-web-git-stg-oyojp.vercel.app/b/B13HNEW/configure?room-id=6&plan-id=18448667&checkin-date=2022-07-22&checkout-date=2022-07-23&room-count=1&guest-count=4_0_0_0_0_0_0');
+  // Click button:has-text("1泊目のプランを選択してください") >> nth=0
+  await page.locator('button:has-text("1泊目のプランを選択してください")').first().click();
+  // Click text=選択する >> nth=0
+  await page.locator('text=選択する').first().click();
+  // Click text=1泊目のプランを確定 >> nth=0
+  await page.locator('text=1泊目のプランを確定').first().click();
+  // Click button:has-text("予約お申し込み手続きへ")
+  await page.locator('button:has-text("予約お申し込み手続きへ")').click();
+  await expect(page).toHaveURL('https://direct-channel-web-git-stg-oyojp.vercel.app/b/B13HNEW/customer-detail');
+  // Click [placeholder="姓"]
+  await page.locator('[placeholder="姓"]').click();
+  // Fill [placeholder="姓"]
+  await page.locator('[placeholder="姓"]').fill('uyen');
+  // Click [placeholder="名"]
+  await page.locator('[placeholder="名"]').click();
+  // Fill [placeholder="名"]
+  await page.locator('[placeholder="名"]').fill('uyen');
+  // Click [placeholder="セイ"]
+  await page.locator('[placeholder="セイ"]').click();
+  // Fill [placeholder="セイ"]
+  await page.locator('[placeholder="セイ"]').fill('uyen');
+  // Click [placeholder="メイ"]
+  await page.locator('[placeholder="メイ"]').click();
+  // Fill [placeholder="メイ"]
+  await page.locator('[placeholder="メイ"]').fill('uyen');
+  // Click [placeholder="〇〇\@tabist\.com"] >> nth=0
+  await page.locator('[placeholder="〇〇\\@tabist\\.com"]').first().click();
+  // Fill [placeholder="〇〇\@tabist\.com"] >> nth=0
+  await page.locator('[placeholder="〇〇\\@tabist\\.com"]').first().fill('uyenntt@tokyotechlab.com');
+  // Fill text=メールアドレス再入力再入力のメールアドレスが一致していません >> [placeholder="〇〇\@tabist\.com"]
+  await page.locator('text=メールアドレス再入力再入力のメールアドレスが一致していません >> [placeholder="〇〇\\@tabist\\.com"]').fill('u');
+  // Click text=メールアドレス再入力再入力のメールアドレスが一致していません >> [placeholder="〇〇\@tabist\.com"]
+  await page.locator('text=メールアドレス再入力再入力のメールアドレスが一致していません >> [placeholder="〇〇\\@tabist\\.com"]').click();
+  // Fill text=メールアドレス再入力再入力のメールアドレスが一致していません >> [placeholder="〇〇\@tabist\.com"]
+  await page.locator('text=メールアドレス再入力再入力のメールアドレスが一致していません >> [placeholder="〇〇\\@tabist\\.com"]').fill('uyenntt@tokyotechlab.com');
+  // Click [placeholder="\30 9012345678"]
+  await page.locator('[placeholder="\\30 9012345678"]').click();
+  // Fill [placeholder="\30 9012345678"]
+  await page.locator('[placeholder="\\30 9012345678"]').fill('123456789');
+  // Click [placeholder="\30 00000"]
+  await page.locator('[placeholder="\\30 00000"]').click();
+  // Fill [placeholder="\30 00000"]
+  await page.locator('[placeholder="\\30 00000"]').fill('123456');
+  // Click [placeholder="都道府県"]
+  await page.locator('[placeholder="都道府県"]').click();
+  // Fill [placeholder="都道府県"]
+  await page.locator('[placeholder="都道府県"]').fill('123456');
+  // Click [placeholder="例：港区海岸1-7-1"]
+  await page.locator('[placeholder="例：港区海岸1-7-1"]').click();
+  // Fill [placeholder="例：港区海岸1-7-1"]
+  await page.locator('[placeholder="例：港区海岸1-7-1"]').fill('12345');
+  // Click textarea
+  await page.locator('textarea').click();
+  // Fill textarea
+  await page.locator('textarea').fill('123456');
+  // Click button:has-text("お支払い方法の選択へ")
+  await page.locator('button:has-text("お支払い方法の選択へ")').click();
+  await expect(page).toHaveURL('https://direct-channel-web-git-stg-oyojp.vercel.app/b/B13HNEW/payment-selection');
+  // Check #pay_later
+  await page.locator('#pay_later').check();
+  // Click text=ゲストポリシー、プライバシーポリシー、利用規約に同意する >> span >> nth=3
+  await page.locator('text=ゲストポリシー、プライバシーポリシー、利用規約に同意する >> span').nth(3).click();
+  // Click button:has-text("お支払い手続きへ")
+  await page.locator('button:has-text("お支払い手続きへ")').click();
+  await expect(page).toHaveURL('https://direct-channel-web-git-stg-oyojp.vercel.app/b/B13HNEW/confirmation');
 });
